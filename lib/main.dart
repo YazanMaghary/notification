b7303e6f4e44e1c63ee43bdf10541d66cd643784
YazanMaghary <zezo2537112@gmail.com>
2023-06-20 15:01:53 +0300

    seconed edit

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:notification/home.dart';
+
 void main(List<String> args) {
   runApp(myApp());
 }
 
+// test
 class myApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build\
-      return  MaterialApp(
-        home: HomePage(),
-      );
+    return MaterialApp(
+      home: HomePage(),
+    );
   }
-  
 }
